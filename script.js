39d45daa975aaa9b03f018fec590d0206ca9ae8b
vnshas <vnshas@gmail.com>
2024-05-20 21:21:42 -0300

    .

--- script.js
+++ script.js
@@ -10,3 +10,46 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(tasks){
+  const myUL = document.getElementsByClassName("tasks__list")
+
+  for(let i = 0;i < tasks.length;i++){
+    
+  }
+}
+
+function createTaskItem(object){
+  const listItem = document.createElement("li")
+  const containerItem = document.createElement("div")
+  const taskSpan = document.createElement("span")
+  const taskTitle = document.createElement("p")
+  const removeTask = document.createElement("button")
+  listItem.classList.toggle("task__item")
+  containerItem.classList.toggle("task-info__container")
+  taskSpan.classList.toggle("task-type")
+  removeTask.classList.toggle("task__button--remove-task")
+
+  
+  
+  const taskType = document.getElementsByClassName("form__input--priority")[0]
+  
+
+  if (taskType.value == "urgente"){
+    taskSpan.classList.toggle("span-urgent")
+  }
+  else if(taskType.value == "importante"){
+    taskSpan.classList.toggle("span-important")
+  }
+  else{
+    taskSpan.classList.toggle("span-normal")
+  }
+
+  listItem.appendChild(containerItem)
+  listItem.appendChild(removeTask)
+  containerItem.appendChild(taskSpan)
+  containerItem.appendChild(taskTitle)
+}
+
+
+renderElements(tasks)

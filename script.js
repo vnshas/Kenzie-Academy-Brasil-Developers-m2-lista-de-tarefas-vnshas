735651edbc7b8e3eb818a26e47dfc281af0b1d39
vnshas <vnshas@gmail.com>
2024-11-22 15:28:50 -0300

    mudando url repositorio img .task__button-remove-task

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tasks = [
-  {title: "Comprar comida para o gato", type: "Urgente"},
+  
+  /*{title: "Comprar comida para o gato", type: "Urgente"},
   {title: "Consertar Computador", type: "Importante"},
   {title: "Beber água", type: "Normal"},
   {title: "Enviar relatório trimestral", type: "Importante"},
@@ -8,19 +9,27 @@ const tasks = [
   {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
   {title: "Limpar a despensa", type: "Importante"},
   {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
+  {title: "Assistir a um documentário interessante", type: "Normal"},*/
 ];
+
+const form = document.querySelector(".form__container")
+
+
 
 function renderElements(tasks){
   const myUL = document.querySelector(".tasks__list")
-  console.log(typeof(myUL))
+  //console.log(typeof(myUL))
   for(let i = 0;i < tasks.length;i++){
+    //console.log(tasks[i].title)
     const newLi = createTaskItem(tasks[i])
-    document.myUL.appendChild(newLi)
+    //console.log(newLi)
+    myUL.appendChild(newLi)
   }
+  
 }
 
 function createTaskItem(object){
+  //console.log(object)
   const listItem = document.createElement("li")
   const containerItem = document.createElement("div")
   const taskSpan = document.createElement("span")
@@ -32,27 +41,68 @@ function createTaskItem(object){
   removeTask.classList.toggle("task__button--remove-task")
 
   
+ 
+  
+  document.addEventListener("click", (e) =>{
+    const targetEl = e.target
+    const parentEL = targetEl.closest("li")
+
+    if (targetEl.classList.contains("task__button--remove-task")){
+      parentEL.remove()
+      
+    }
+  
+  })
+    
+
+  
   
   const taskType = object.type
+ 
+ 
+  if (taskType == "Urgente"){
+      taskSpan.classList.toggle("span-urgent")
+  }
+  else if(taskType == "Importante"){
+      taskSpan.classList.toggle("span-important")
+  }
+  else if (taskType == "Normal"){
+      taskSpan.classList.toggle("span-normal")
+  }
   
-
-  if (taskType == "urgente"){
-    taskSpan.classList.toggle("span-urgent")
-  }
-  else if(taskType == "importante"){
-    taskSpan.classList.toggle("span-important")
-  }
-  else{
-    taskSpan.classList.toggle("span-normal")
-  }
+  
 
   taskTitle.innerText = object.title
 
   listItem.appendChild(containerItem)
-  listItem.appendChild(removeTask)
   containerItem.appendChild(taskSpan)
   containerItem.appendChild(taskTitle)
+  listItem.appendChild(removeTask)
+
+  
+  //const myUL = document.querySelector(".tasks__list")
+  return listItem
+
+  
 }
 
 
-renderElements(tasks)
+
+
+
+form.addEventListener("submit",function(event){
+  event.preventDefault()
+
+  const inputTask = document.querySelector(".form__input--text")
+  const selectTask = document.querySelector(".form__input--priority")
+  
+ 
+  tasks.push({"title":inputTask.value, "type": selectTask.value})
+  
+  
+  renderElements(tasks)
+  //console.log(tasks)
+  tasks.pop()
+  
+})
+
